97f3186ac8e4e9ba9ef50c037f5ae6f0f64f10ac
Andrea Sanchez <l19141196@queretaro.tecnm.mx>
2023-05-07 08:39:53 -0600

    cambio a php

---
+++ include/footer.php
@@ -0,0 +1,34 @@
+<footer class="justify-content-between py-3 border-top">
+    <!-- app -->
+    <div class="container text-center">
+      <div class="row justify-content-md-center row-cols-2">
+        <div class="col-6">
+          <p class="text-white fs-5"><strong>Descarga la app de volaris</strong></p>
+          <ul class="nav justify-content-center list-unstyled">
+            <li class="ms-3"><a class="google" href="http://bit.ly/1wvn7WN" target="_blank"><img src="img/googlePlay.svg"
+                  height="50px" alt="google Play"></a></li>
+            <li class="ms-3"><a class="store" href="https://itunes.apple.com/us/app/volaris/id945390888?ls=1&amp;mt=8"
+                target="_blank"><img src="img/appStore.svg" height="50px" alt="Itunes"></a></li>
+          </ul>
+        </div>
+        <!-- Redes -->
+        <div class="col-6">          
+          <p class="text-white fs-5"><strong>Siguenos en redes sociales</strong></p>
+          <ul class="nav justify-content-center list-unstyled d-flex rounded-social-buttons">
+            <li class="ms-3"><a class="social-button facebook" href="https://www.facebook.com/viajavolaris"
+                target="_blank"><i class="bi bi-facebook"></i></a></li>
+            <li class="ms-3"><a class="social-button twitter" href="https://twitter.com/viajaVolaris/" target="_blank"><i
+                  class="bi bi-twitter"></i></a></li>
+            <li class="ms-3"><a class="social-button linkedin"
+                href="https://www.linkedin.com/authwall?trk=ripf&amp;trkInfo=AQHPxoMxdaLCRQAAAW6Ne2LAKePLAk9ymi5vP9zBL9Etvf3pwrYCwj89Zef33uCBAvXM0dln_hpBOtmcArvq6-oYgmtETeoHAgwHc0vhI0jrhsfemJJxO0C7WqC2VaU_KYpirLg=&amp;originalReferer=https://www.volaris.com/?culture=es-MX&amp;currency=USD&amp;sessionRedirect=https:%2F%2Fwww.linkedin.com%2Fcompany%2Fvolaris"
+                target="_blank"><i class="bi bi-linkedin"></i></a></li>
+            <li class="ms-3"><a class="social-button whatsApp"
+                href="https://wa.me/5215558988599?text=Hola,%20%C2%BFme%20pueden%20ayudar?%20" target="_blank"><i
+                  class="bi bi-whatsapp"></i></a></li>
+            <li class="ms-3"><a class="social-button instagram" href="https://www.instagram.com/viajavolaris/"
+                target="_blank"><i class="bi bi-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>  
+  </footer>
---
+++ include/header.php
@@ -0,0 +1,37 @@
+
+  <!-- Responsive navbar-->
+  <div class="container header_logo">
+    <div class="logo">
+      <a class="navbar-brand" href="index.php">
+        <img id="volaris-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Volaris-logo.svg/2560px-Volaris-logo.svg.png"
+          alt="volaris Logo" draggable="false" height="30" />
+      </a>
+    </div>
+    <div class="moneda">
+      <span class="btn-moneda">
+        <select>
+          <option>Pesos Mexicanos - MXN</option>
+          <option>Dolares Americanos - USD</option>
+          <option>Euros - EUR</option>
+        </select>
+      </span>
+    </div>
+  </div>
+  
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><b>Vuelos</b></a></li>
+          <li class="nav-item"><a class="nav-link" href="Reserva.html">Mi reserva</a></li>
+          <li class="nav-item"><a class="nav-link" href="estatus.html">Estatus de vuelo</a></li>
+          <li class="nav-item"><a class="nav-link" href="contacto.html">Contacto</a></li>
+          <li class="nav-item"><a class="nav-link" href="volaris.html">Sobre volaris</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
